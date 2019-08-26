c3b6d47af325d9c587a0c855d3640b7977ff53fc
Ethan Collins <ethantcollins98@gmail.com>
2019-08-26 13:02:35 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const gifts = ['teddy bear', 'drone', 'doll'];
+
+function wrapGift(gifts) {
+    for (let i = 0; i < 3; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts
+}
+
+function writeCards(names, eventName) {
+    const thanksMessages = []
+    for (let i = 0; i < names.length; i++) {
+        thanksMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return thanksMessages
+}
+
+function countdown(startingNum) {
+    for (let i = startingNum; i >= 0; i--){
+        console.log(i)
+    }
+}
+
+wrapGifts(gifts)
+
+
